f17ebb2480ebf58ec25012f4c1b4fa10e61f15b0
atdalman <atdalman@gmail.com>
2019-11-17 16:56:37 +0900

    Added in properties for saving throws

--- src/main/java/osr/monsterGenerator/mechanicalProperties/SavingThrow.java
+++ src/main/java/osr/monsterGenerator/mechanicalProperties/SavingThrow.java
@@ -1,4 +1,14 @@
 package osr.monsterGenerator.mechanicalProperties;
 
 public class SavingThrow {
+    private String name;
+    private int value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
