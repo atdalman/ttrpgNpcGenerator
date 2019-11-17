b980ef877dc44be70f9a7dba1934c39e7c35ab02
atdalman <atdalman@gmail.com>
2019-11-17 16:32:21 +0900

    Added in properties for physical attack

--- src/main/java/osr/monsterGenerator/mechanicalProperties/PhysicalAttack.java
+++ src/main/java/osr/monsterGenerator/mechanicalProperties/PhysicalAttack.java
@@ -1,4 +1,41 @@
 package osr.monsterGenerator.mechanicalProperties;
 
 public class PhysicalAttack {
+
+    private String name; // claw/bite/strike
+    private int flatDamage; // average damage
+    private String damageRoll; // d4/d6/etc
+    private String specialEffect; //
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFlatDamage() {
+        return flatDamage;
+    }
+
+    public void setFlatDamage(int flatDamage) {
+        this.flatDamage = flatDamage;
+    }
+
+    public String getDamageRoll() {
+        return damageRoll;
+    }
+
+    public void setDamageRoll(String damageRoll) {
+        this.damageRoll = damageRoll;
+    }
+
+    public String getSpecialEffect() {
+        return specialEffect;
+    }
+
+    public void setSpecialEffect(String specialEffect) {
+        this.specialEffect = specialEffect;
+    }
 }
--- src/main/java/osr/monsterGenerator/npc/Monster.java
+++ src/main/java/osr/monsterGenerator/npc/Monster.java
@@ -6,7 +6,7 @@ import osr.monsterGenerator.mechanicalProperties.SavingThrow;
 public class Monster {
     private String alignment;
     private String name; // Species name, for example
-    private String properName; // Use if it is a named Monster
+    private String properName; // Use if it is a named Monster.  Maybe a name generator in the future?
     private String armorClass;
     private int healthPoints;
     private int hitDie;
@@ -25,5 +25,163 @@ public class Monster {
     private SavingThrow[] savingThrows;
     private String morale;
 
+    public String getAlignment() {
+        return alignment;
+    }
 
+    public void setAlignment(String alignment) {
+        this.alignment = alignment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProperName() {
+        return properName;
+    }
+
+    public void setProperName(String properName) {
+        this.properName = properName;
+    }
+
+    public String getArmorClass() {
+        return armorClass;
+    }
+
+    public void setArmorClass(String armorClass) {
+        this.armorClass = armorClass;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getHitDie() {
+        return hitDie;
+    }
+
+    public void setHitDie(int hitDie) {
+        this.hitDie = hitDie;
+    }
+
+    public int getExperienceGiven() {
+        return experienceGiven;
+    }
+
+    public void setExperienceGiven(int experienceGiven) {
+        this.experienceGiven = experienceGiven;
+    }
+
+    public String getGeneralBodyShape() {
+        return generalBodyShape;
+    }
+
+    public void setGeneralBodyShape(String generalBodyShape) {
+        this.generalBodyShape = generalBodyShape;
+    }
+
+    public String getBodyCharacterstics() {
+        return bodyCharacterstics;
+    }
+
+    public void setBodyCharacterstics(String bodyCharacterstics) {
+        this.bodyCharacterstics = bodyCharacterstics;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getMovementStyle() {
+        return movementStyle;
+    }
+
+    public void setMovementStyle(String movementStyle) {
+        this.movementStyle = movementStyle;
+    }
+
+    public int getMovementSpeed() {
+        return movementSpeed;
+    }
+
+    public void setMovementSpeed(int movementSpeed) {
+        this.movementSpeed = movementSpeed;
+    }
+
+    public String getDistinctiveFeatures() {
+        return distinctiveFeatures;
+    }
+
+    public void setDistinctiveFeatures(String distinctiveFeatures) {
+        this.distinctiveFeatures = distinctiveFeatures;
+    }
+
+    public PhysicalAttack[] getPhysicalAttacks() {
+        return physicalAttacks;
+    }
+
+    public void setPhysicalAttacks(PhysicalAttack[] physicalAttacks) {
+        this.physicalAttacks = physicalAttacks;
+    }
+
+    public String[] getSpecialAbilities() {
+        return specialAbilities;
+    }
+
+    public void setSpecialAbilities(String[] specialAbilities) {
+        this.specialAbilities = specialAbilities;
+    }
+
+    public String getCombatStrategyPrimary() {
+        return combatStrategyPrimary;
+    }
+
+    public void setCombatStrategyPrimary(String combatStrategyPrimary) {
+        this.combatStrategyPrimary = combatStrategyPrimary;
+    }
+
+    public String getCombatStrategySecondary() {
+        return combatStrategySecondary;
+    }
+
+    public void setCombatStrategySecondary(String combatStrategySecondary) {
+        this.combatStrategySecondary = combatStrategySecondary;
+    }
+
+    public String getMotivation() {
+        return motivation;
+    }
+
+    public void setMotivation(String motivation) {
+        this.motivation = motivation;
+    }
+
+    public SavingThrow[] getSavingThrows() {
+        return savingThrows;
+    }
+
+    public void setSavingThrows(SavingThrow[] savingThrows) {
+        this.savingThrows = savingThrows;
+    }
+
+    public String getMorale() {
+        return morale;
+    }
+
+    public void setMorale(String morale) {
+        this.morale = morale;
+    }
 }
