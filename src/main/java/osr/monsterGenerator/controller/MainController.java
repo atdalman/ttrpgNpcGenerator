1e600c2fd603758797a3f3e74f80494be9bd72b2
atdalman <atdalman@gmail.com>
2020-06-17 14:17:54 +0900

    Added terminal stylesheets and logic.  Created main controller.

---
+++ src/main/java/osr/monsterGenerator/controller/MainController.java
@@ -0,0 +1,14 @@
+package osr.monsterGenerator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("eventName", "FIFA 2018");
+        return "index";
+    }
+}
