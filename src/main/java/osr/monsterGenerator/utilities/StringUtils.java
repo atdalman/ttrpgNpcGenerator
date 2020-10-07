a47b81981afca296cac03654d3e017a4cdb1c98b
atdalman <atdalman@gmail.com>
2020-10-07 15:06:56 +0900

    Removed toCamelCaseFromTitleCase.  Not being used.

--- src/main/java/osr/monsterGenerator/utilities/StringUtils.java
+++ src/main/java/osr/monsterGenerator/utilities/StringUtils.java
@@ -12,15 +12,4 @@ public class StringUtils {
 
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
-
-    // TODO May not be needed, as MongoTemplate.getCollectionName() fulfills the same function
-    public static String toCamelCaseFromTitleCase(String word) {
-        if (word == null || word.length() == 0) return "";
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(Character.toLowerCase(word.charAt(0)));
-        sb.append(word.substring(1));
-
-        return sb.toString();
-    }
 }
