3004168c291205a2da2d43393c117aa264cf665e
atdalman <atdalman@gmail.com>
2020-10-22 11:55:49 +0900

    Started writing test classes for endpoints.

--- src/test/java/osr/monsterGenerator/restTests/SystemsControllerTest.java
+++ src/test/java/osr/monsterGenerator/restTests/SystemsControllerTest.java
@@ -9,12 +9,10 @@ public class SystemsControllerTest {
     @Test
     public void getAllSystems_200() {
         given().when().get("http://localhost:8080/api/systems").then().assertThat().statusCode(200);
-
     }
 
     @Test
     public void getAllSystems_() {
         given().when().get("http://localhost:8080/api/systems").then().assertThat().statusCode(200);
-
     }
 }
