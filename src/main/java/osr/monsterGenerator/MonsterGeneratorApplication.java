7deff2e082b35de9bc79b8dd531358ad3fdd5ea8
atdalman <atdalman@gmail.com>
2020-11-02 11:46:29 +0900

    Added sample embedded mongodb test.

--- src/main/java/osr/monsterGenerator/MonsterGeneratorApplication.java
+++ src/main/java/osr/monsterGenerator/MonsterGeneratorApplication.java
@@ -1,20 +1,12 @@
 package osr.monsterGenerator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import osr.monsterGenerator.repository.AttributeDAO;
-import osr.monsterGenerator.service.NPCFactory;
 
 @SpringBootApplication
 public class MonsterGeneratorApplication implements CommandLineRunner {
 
-	@Autowired
-	private NPCFactory npcFactory;
-
-	@Autowired
-	private AttributeDAO attributeDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonsterGeneratorApplication.class, args);
